150af7175b6afa0adcdc21d60e2e0efe34f89604
yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>
2024-05-10 22:13:30 +0530

    Update live.py

--- MikuXMusic/plugins/play/live.py
+++ MikuXMusic/plugins/play/live.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.stream.stream import stream
+from MikuXMusic import YouTube, app
+from MikuXMusic.utils.channelplay import get_channeplayCB
+from MikuXMusic.utils.decorators.language import languageCB
+from MikuXMusic.utils.stream.stream import stream
 from config import BANNED_USERS
 
 
