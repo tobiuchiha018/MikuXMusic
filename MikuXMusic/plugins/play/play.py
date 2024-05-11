09c673fdf4c3ee8c047171a9091a7633394b06d0
yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>
2024-05-11 06:56:59 +0530

    Update play.py

--- MikuXMusic/plugins/play/play.py
+++ MikuXMusic/plugins/play/play.py
@@ -6,22 +6,22 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import seconds_to_min, time_to_seconds
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.decorators.play import PlayWrapper
-from AnonXMusic.utils.formatters import formats
-from AnonXMusic.utils.inline import (
+from MikuXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from MikuXMusic.core.call import Anony
+from MikuXMusic.utils import seconds_to_min, time_to_seconds
+from MikuXMusic.utils.channelplay import get_channeplayCB
+from MikuXMusic.utils.decorators.language import languageCB
+from MikuXMusic.utils.decorators.play import PlayWrapper
+from MikuXMusic.utils.formatters import formats
+from MikuXMusic.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from AnonXMusic.utils.logger import play_logs
-from AnonXMusic.utils.stream.stream import stream
+from MikuXMusic.utils.logger import play_logs
+from MikuXMusic.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
